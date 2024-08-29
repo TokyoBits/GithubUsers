9795e3abc464acad9269c2db1d13b43e85688d82
Brett Gneiting <brett@tokyobits.jp>
2024-08-29 21:50:05 +0900

    Update design for User Detail area

--- GithubUsers/Screens/UserDetailScreen.swift
+++ GithubUsers/Screens/UserDetailScreen.swift
@@ -13,28 +13,66 @@ struct UserDetailScreen: View {
     @State private var user: User?
 
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: user?.userImage ?? "")) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .clipShape(.circle)
-            } placeholder: {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .clipShape(.circle)
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                AsyncImage(url: URL(string: user?.userImage ?? "")) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(.circle)
+                } placeholder: {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipShape(.circle)
+                }
             }
-            Text(user?.fullName ?? "full")
-            Text(user?.bio ?? "bio")
-            Text("Company: \(user?.company ?? "company") ")
-            Text(user?.location ?? "location")
-            Text("\(user?.followersCount ?? 0) followers")
-            Text("\(user?.followingCount ?? 0) following")
+            .frame(maxWidth: .infinity)
+            Group {
+                Text(user?.username ?? "")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user?.fullName ?? "")
+                    .font(.title)
+                Text(user?.bio ?? "")
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+            }
+            HStack {
+                if let company = user?.company {
+                    Label(company, systemImage: "building.fill")
+                }
+                Spacer()
+                if let location = user?.location {
+                    Label(location, systemImage: "mappin.and.ellipse")
+                }
+            }
+            .padding()
+            .font(.caption)
+            HStack(spacing: 2) {
+                Image(systemName: "person.2.fill")
+                Text("\(user?.followersCount ?? 0)")
+                    .bold()
+                Text("followers")
+                Text("~")
+                Text("\(user?.followingCount ?? 0)")
+                    .bold()
+                Text("following")
+            }
+            .frame(maxWidth: .infinity)
+            .font(.caption)
+        }
+        .padding()
+        .background(.gray.opacity(0.3))
+        .clipShape(.rect(cornerRadius: 15))
+        .padding()
+        ScrollView {
+            Section("Repositories") {
+                RepositoryListView(username: username)
+            }
 
-            RepositoryListView(username: username)
         }
         .task {
             do {
@@ -44,7 +82,6 @@ struct UserDetailScreen: View {
                 print(error)
             }
         }
-        .navigationTitle(username)
     }
 }
 
