6e6d5ab1f6d296a70fea83d1affb79520dbb95ad
Brett Gneiting <brett@tokyobits.jp>
2024-09-03 00:30:59 +0900

    Cleanup behavior on Users List and add paging

--- GithubUsers/Screens/UserList/UserListScreen.swift
+++ GithubUsers/Screens/UserList/UserListScreen.swift
@@ -6,23 +6,32 @@
 //
 
 import SwiftUI
+import OSLog
 
 struct UserListScreen: View {
+    let logger = Logger(subsystem: "jp.tokyobits.githubusers", category: "UserListScreen")
+
     @State private var networkManager = NetworkManager.shared
     @State private var users: [User] = []
 
+    @State private var isLoading: Bool = true
+    @State private var usersPerPage: Int = 30
+
+    @State private var userSince: Int = 0
+
     var body: some View {
         Group {
-            if users.isEmpty {
+            if users.isEmpty && !isLoading {
                 unavailableView
             } else {
-                usersListView
-                    .navigationDestination(for: User.self) { user in
-                        UserDetailScreen(username: user.username)
-                    }
-                    .refreshable {
-                        await fetchUsers()
-                    }
+                if isLoading {
+                    initialLoadingView
+                } else {
+                    usersListView
+                        .navigationDestination(for: User.self) { user in
+                            UserDetailScreen(username: user.username)
+                        }
+                }
             }
         }
         .task {
@@ -46,28 +55,64 @@ struct UserListScreen: View {
         }
     }
 
+    private var initialLoadingView: some View {
+        ContentUnavailableView {
+            Image(systemName: "person.crop.circle.dashed.circle")
+                .font(.system(size: 40))
+                .symbolEffect(.pulse)
+                .foregroundStyle(.gray)
+            Text("Loading Users")
+                .font(.title2)
+                .fontWeight(.semibold)
+        } description: {
+            Text("Please wait while data is loading.")
+        } actions: {
+            Button("Reload") {
+                Task {
+                    await fetchUsers()
+                }
+            }
+            .buttonStyle(.borderedProminent)
+        }
+    }
+
     private var usersListView: some View {
         List {
-            ForEach(users) { user in
-                NavigationLink(value: user) {
-                    HStack {
-                        AsyncImage(url: URL(string: user.userImage)) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 40, height: 40)
-                                .circularImage()
-                        } placeholder: {
-                            Image(systemName: "person.circle.fill")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 40, height: 40)
-                                .circularImage()
+            Section {
+                ForEach(users) { user in
+                    NavigationLink(value: user) {
+                        HStack {
+                            AsyncImage(url: URL(string: user.userImage)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 40, height: 40)
+                                    .circularImage()
+                            } placeholder: {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 40, height: 40)
+                                    .circularImage()
+                            }
+                            Text(user.username)
+                            Text("\(user.id)")
                         }
-                        Text(user.username)
+                        .listRowSeparator(.hidden)
                     }
-                    .listRowSeparator(.hidden)
                 }
+            } footer: {
+                Button {
+                    Task {
+                        await fetchUsers()
+                    }
+                } label: {
+                    Text("Load More Users")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
             }
         }
         .listStyle(.plain)
@@ -75,7 +120,15 @@ struct UserListScreen: View {
 
     private func fetchUsers() async {
         do {
-            users = try await networkManager.fetchUsers()
+            logger.debug("Fetching users since \(userSince), perPage \(usersPerPage)")
+            let fetchedUsers = try await networkManager.fetchUsers(since: userSince, perPage: usersPerPage)
+            users.append(contentsOf: fetchedUsers)
+
+            guard let lastId = users.last?.id else { return }
+            logger.debug("Last user id: \(lastId)")
+            userSince = lastId
+
+            isLoading = false
         } catch {
             print(error)
         }
